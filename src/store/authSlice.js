44d4e30a5075f3556e521655a12c186ed90b1ef0
Avinash Yadav <avinashurmilayadav@gmail.com>
2024-04-05 02:06:08 +0530

    Updated

---
+++ src/store/authSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+
+const initialState = {
+    status: false,
+    useData: null
+}
+
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        login: (state, action) => {
+            state.status = true;
+            state.userData = action.payload.userData;
+        },
+
+        logout: (state) => {
+            state.status = false;
+            state.useData = null;
+        }
+    }
+});
+
+
+export const { login, logout } = authSlice.actions;
+
+
+export default authSlice.reducer;
