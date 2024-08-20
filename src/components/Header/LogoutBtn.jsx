ee59d388dd03c9b9178b8a764071e52f7af09ae9
Avinash Yadav <avinashurmilayadav@gmail.com>
2024-08-20 22:44:20 +0530

    Updates

---
+++ src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,26 @@
+import { useDispatch } from "react-redux";
+import authService from "../../appwrite/auth"; // To check it once keeping it for later
+import { logout } from "../../store/authSlice";
+
+function LogoutBtn() {
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    authService
+      .logout()
+      .then(() => {
+        dispatch(logout());
+      })
+      .catch((err) => alert(err.message));
+  };
+
+  return (
+    <>
+      <button className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full">
+        Log Out
+      </button>
+    </>
+  );
+}
+
+export default LogoutBtn;
